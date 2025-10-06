ff98089af83fde0916f3e67e6365420279497736
MarinDev101 <186996015+MarinDev101@users.noreply.github.com>
2025-10-06 17:27:26 -0500

    feat: añadir base para pagina inicio

---
+++ frontend/src/app/pages/inicio/inicio.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { HeaderBasico } from '@app/components/headers/header-basico/header-basico';
+
+@Component({
+  selector: 'app-inicio',
+  imports: [HeaderBasico],
+  templateUrl: './inicio.html',
+})
+export class Inicio {}
