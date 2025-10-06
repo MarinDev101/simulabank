a66a7db1cadb890a09f3bb922df2d191d05a65e5
MarinDev101 <186996015+MarinDev101@users.noreply.github.com>
2025-10-06 17:32:26 -0500

    feat: añadir importaciones de componente inicio y configuracion de estilos globales

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Inicio } from './pages/inicio/inicio';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.html'
+  imports: [RouterOutlet, Inicio],
+  templateUrl: './app.html',
 })
 export class App {
   protected title = 'frontend';
