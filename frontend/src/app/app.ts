973cc2a165723d2cca4d6e74cf8177e4a56e5d7d
Avi004 <juansgamer1@gmail.com>
2025-10-06 17:31:47 -0500

    feat: añadir rutas de pages

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ConfiguracionSimulacion } from './pages/configuracion-simulacion/configuracion-simulacion';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.html'
+  imports: [RouterOutlet, ConfiguracionSimulacion],
+  templateUrl: './app.html',
 })
 export class App {
   protected title = 'frontend';
