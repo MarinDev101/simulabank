88d02231e8a89e56c3985cdb664de02a1db8839d
MarinDev101 <186996015+MarinDev101@users.noreply.github.com>
2025-10-06 17:31:21 -0500

    feat: añadir base para componente header basico

---
+++ frontend/src/app/components/headers/header-basico/header-basico.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-basico',
+  imports: [],
+  templateUrl: './header-basico.html'
+})
+export class HeaderBasico {
+
+}
