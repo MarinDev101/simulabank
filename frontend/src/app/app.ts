0762b73d020338d8f15ee9053d717581b71e01ca
Avi004 <juansgamer1@gmail.com>
2025-10-15 17:35:12 -0500

    fix: solucionar importacion no utilizada

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ConfiguracionSimulacion } from './pages/configuracion-simulacion/configuracion-simulacion';
 import { InformacionPlataforma } from "./pages/informacion-plataforma/informacion-plataforma";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ConfiguracionSimulacion, InformacionPlataforma],
+  imports: [RouterOutlet, InformacionPlataforma],
   templateUrl: './app.html',
 })
 export class App {
