ebd04e6ffe358f0b602288164c4ee40a11eaf528
Avi004 <juansgamer1@gmail.com>
2025-10-15 17:21:05 -0500

    feat: actualizando app

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ConfiguracionSimulacion } from './pages/configuracion-simulacion/configuracion-simulacion';
+import { InformacionPlataforma } from "./pages/informacion-plataforma/informacion-plataforma";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ConfiguracionSimulacion],
+  imports: [RouterOutlet, ConfiguracionSimulacion, InformacionPlataforma],
   templateUrl: './app.html',
 })
 export class App {
