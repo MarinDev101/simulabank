e4680adea97b9305068edf46994670c17868fa29
MarinDev101 <186996015+MarinDev101@users.noreply.github.com>
2025-10-17 10:42:16 -0500

    feat: añadir pagina de inicio

--- frontend/src/app/pages/inicio/inicio.ts
+++ frontend/src/app/pages/inicio/inicio.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { HeaderBasico } from '@app/components/headers/header-basico/header-basico';
+import { GoToRegisterButton } from '@app/components/go-to-register-button/go-to-register-button';
+import { Footer } from '@app/core/layout/footer/footer';
+import { HeaderBasico } from '@app/core/layout/headers/header-basico/header-basico';
 
 @Component({
   selector: 'app-inicio',
-  imports: [HeaderBasico],
+  standalone: true,
+  imports: [HeaderBasico, Footer, GoToRegisterButton],
   templateUrl: './inicio.html',
 })
 export class Inicio {}
